8f0fc4702a8cee5c9e699f2bc43cb32199228f51
root <root@li1272-103.members.linode.com>
2020-05-18 19:40:35 +0000

    Got over one big hump!

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -7,7 +7,7 @@ const ListTodos = () => {
    
    const deleteTodo = async (id) => {
      try {
-       const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+       const deleteTodo = await fetch(`http://45.79.173.103:5000/todos/${id}`, {
          method: "DELETE"
        })
 
@@ -20,7 +20,7 @@ const ListTodos = () => {
 
    const getTodos = async () => {
     try {
-      const response = await fetch("http://localhost:5000/todos")
+      const response = await fetch("http://45.79.173.103:5000/todos")
       const jsonData = await response.json()
 
       console.log(response)
